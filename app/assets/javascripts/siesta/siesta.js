726bdf8b3d551d19f35d04dcb5bf72af9882fe7d
Tower He <towerhe@gmail.com>
2012-07-15 01:27:36 +0800

    Default settings of siesta

---
+++ app/assets/javascripts/siesta/siesta.js
@@ -0,0 +1,9 @@
+Siesta.Harness.Browser.ExtJS.configure({
+  title: 'ExtJS Application',
+  loaderPath: { 'ExtJS' : '/assets/extjs/app' },
+
+  preload : [
+    "/assets/extjs/resources/css/ext-all.css",
+    "/assets/extjs/ext-all-debug.js"
+  ]
+});
