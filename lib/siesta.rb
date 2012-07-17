9b55640236ab9b1dfbc4ba8b5de21b4bc425164b
Tower He <towerhe@gmail.com>
2012-07-18 03:05:29 +0800

    Config the directory of spec.

--- lib/siesta.rb
+++ lib/siesta.rb
@@ -1,4 +1,13 @@
 require "siesta/engine"
 
 module Siesta
+  class << self
+    def config
+      @config ||= ActiveSupport::OrderedOptions.new
+    end
+
+    def configure
+      yield config
+    end
+  end
 end
