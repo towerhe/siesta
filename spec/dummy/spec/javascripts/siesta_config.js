a02749bd7d0a9e272178e6446cd98e1312d1855e
Tower He <towerhe@gmail.com>
2012-07-18 11:58:13 +0800

    Created a customized config file.

---
+++ spec/dummy/spec/javascripts/siesta_config.js
@@ -0,0 +1,9 @@
+Siesta.Harness.Browser.ExtJS.configure({
+  title: 'Dummy App',
+  loaderPath: { 'DummyApp' : '/assets/extjs/app' },
+
+  preload : [
+    "/assets/extjs/resources/css/ext-all.css",
+    "/assets/extjs/ext-all-debug.js"
+  ]
+});
